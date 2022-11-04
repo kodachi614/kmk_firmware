7d43f439f84f68165c5def01ea2be1f08905e092
Flynn <flynn+github@kodachi.com>
2022-11-04 17:12:23 -0400

    Support setting pull mode for rotary encoder

--- kmk/scanners/encoder.py
+++ kmk/scanners/encoder.py
@@ -2,14 +2,22 @@ import keypad
 import rotaryio
 
 from kmk.scanners import Scanner
+from kmk.utils import Debug
 
+debug = Debug(__name__)
 
 class RotaryioEncoder(Scanner):
-    def __init__(self, pin_a, pin_b, divisor=4):
-        self.encoder = rotaryio.IncrementalEncoder(pin_a, pin_b, divisor)
+    def __init__(self, pin_a, pin_b, divisor=4, pull="up"):
+        debug("Initializing rotaryio encoder")
+        self.pin_a = pin_a
+        self.pin_b = pin_b
+        self.encoder = rotaryio.IncrementalEncoder(pin_a, pin_b, divisor=divisor, pull=pull)
         self.position = 0
         self._pressed = False
         self._queue = []
+
+    def __str__(self):
+        return f"RotaryioEncoder({self.pin_a}/{self.pin_b})"
 
     @property
     def key_count(self):
@@ -19,6 +27,9 @@ class RotaryioEncoder(Scanner):
         position = self.encoder.position
 
         if position != self.position:
+            if debug.enabled:
+                debug(f"Encoder {self} changed from {self.position} to {position}")
+
             self._queue.append(position - self.position)
             self.position = position
 
